2893ddbc5b26b480e2ea19706a2b418766e3e2fe
Seba <msebastian86@gmail.com>
2021-02-07 12:22:34 +0100

    improved burger menu

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -60,13 +60,13 @@ export default {
 <style scoped lang="scss">
 @keyframes slide-top {
   0% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
+    -webkit-transform: translateY(100px);
+    transform: translateY(100px);
     opacity: 0;
   }
   100% {
-    -webkit-transform: translateY(-100px);
-    transform: translateY(-100px);
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
     opacity: 1;
   }
 }
@@ -152,6 +152,7 @@ export default {
   align-content: center;
   justify-content: center;
   flex-direction: center;
+  touch-action: none;
 
   .burger-menu__container {
     display: flex;
